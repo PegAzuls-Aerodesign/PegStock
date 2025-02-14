fc791337a38fdc00246e55371c2537192a99d2bd
lauramoroni <moronilaura28@gmail.com>
2025-02-14 16:30:46 -0300

    initialize PegStock project

---
+++ src/main/java/com/pegazuls/aerodesign/PegStock/PegStockApplication.java
@@ -0,0 +1,13 @@
+package com.pegazuls.aerodesign.PegStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pegazuls/aerodesign/PegStock/PegStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pegazuls.aerodesign.PegStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PegStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
