ed12657e867f5ad0368e2ea54abb38d1f1071667
ClaraLeticia <claralet1346@gmail.com>
2025-02-21 16:05:52 -0300

    fix setters and getters Material

--- src/main/java/com/pegazuls/aerodesign/PegStock/model/entities/Material.java
+++ src/main/java/com/pegazuls/aerodesign/PegStock/model/entities/Material.java
@@ -8,12 +8,14 @@ import com.pegazuls.aerodesign.PegStock.model.enums.*;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Table
 @Entity(name = "tb_material")
 @Getter
 @Setter
+@AllArgsConstructor
 public class Material {
 
    @Id
@@ -39,105 +41,8 @@ public class Material {
    @OneToMany(mappedBy = "material", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
    private List<Borrowing> borrowing;
 
-   public Material(Long cod, String name, String description, int quantity, Category category, Box box, LocalDate expirationDate, LocalDate registerDate, List<Borrowing> borrowing) {
-      this.cod = cod;
-      this.name = name;
-      this.description = description;
-      this.quantity = quantity;
-      this.category = category;
-      this.box = box;
-      this.expirationDate = expirationDate;
-      this.createdDate = LocalDate.now();
-      this.registerDate = registerDate;
-      this.borrowing = borrowing;
-   }
-
    public Material() {
       registerDate = LocalDate.now();
    }
-
-   public Material(Object o, LocalDate now, LocalDate now1, LocalDate now2, Box box, Category category, int i, String s, String s1) {
-   }
-
-   public List<Borrowing> getBorrowing() {
-      return borrowing;
-   }
-
-   public void setBorrowing(List<Borrowing> borrowing) {
-      this.borrowing = borrowing;
-   }
-
-   public LocalDate getRegisterDate() {
-      return registerDate;
-   }
-
-   public void setRegisterDate(LocalDate registerDate) {
-      this.registerDate = registerDate;
-   }
-
-   public LocalDate getCreatedDate() {
-      return createdDate;
-   }
-
-   public void setCreatedDate(LocalDate createdDate) {
-      this.createdDate = createdDate;
-   }
-
-   public LocalDate getExpirationDate() {
-      return expirationDate;
-   }
-
-   public void setExpirationDate(LocalDate expirationDate) {
-      this.expirationDate = expirationDate;
-   }
-
-   public Box getBox() {
-      return box;
-   }
-
-   public void setBox(Box box) {
-      this.box = box;
-   }
-
-   public Category getCategory() {
-      return category;
-   }
-
-   public void setCategory(Category category) {
-      this.category = category;
-   }
-
-   public int getQuantity() {
-      return quantity;
-   }
-
-   public void setQuantity(int quantity) {
-      this.quantity = quantity;
-   }
-
-   public String getDescription() {
-      return description;
-   }
-
-   public void setDescription(String description) {
-      this.description = description;
-   }
-
-   public String getName() {
-      return name;
-   }
-
-   public void setName(String name) {
-      this.name = name;
-   }
-
-   public Long getCod() {
-      return cod;
-   }
-
-   public void setCod(Long cod) {
-      this.cod = cod;
-   }
-
 
 }
