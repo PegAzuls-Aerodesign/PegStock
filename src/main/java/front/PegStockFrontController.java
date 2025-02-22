7576ba65389e2cdeb3aea205527fa550520bd4c9
Lucas Silva de Souza <lucassilv0111@gmail.com>
2025-02-22 18:33:54 -0300

    Att stock page and creation register page

--- src/main/java/front/PegStockFrontController.java
+++ src/main/java/front/PegStockFrontController.java
@@ -2,9 +2,14 @@ package front;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class PegStockFrontController {
 
@@ -24,10 +29,25 @@ public class PegStockFrontController {
     private Button buttonStock;
 
     @FXML
+    private Button buttonNavigate;
+
+    @FXML
+    private Button buttonRegister;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
     private Text nameOut;
 
     @FXML
     private AnchorPane stockPage;
+
+    @FXML
+    private AnchorPane registerPage;
+
+    @FXML
+    private AnchorPane registerPageBackground;
 
     @FXML
     void pages(MouseEvent event) {
@@ -44,6 +64,26 @@ public class PegStockFrontController {
         } else if (event.getSource() == buttonLoan) {
             buttonLoan.setOnMouseClicked(event1 -> nameOut.setText(buttonLoan.getText()));
         }
+    }
+
+    @FXML
+    void selectImage(MouseEvent event) {
+        // Busca a imagem do botão clicado e exibe na tela
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg"));
+
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null) {
+            Image image = new Image(file.toURI().toString());
+            imageView.setImage(image);
+        }
+    }
+
+    @FXML
+    void showRegisterPage(MouseEvent event) {
+        // Exibe a página de registro
+        buttonRegister.setOnMouseClicked(event1 -> registerPageBackground.setVisible(true));
+        buttonRegister.addEventHandler(MouseEvent.MOUSE_CLICKED, event1 -> registerPage.setVisible(true));
     }
 
 }
