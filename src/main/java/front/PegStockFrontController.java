1167dbfd84cf59ec1bbdb31773b42fe6981f246a
Lucas Silva de Souza <lucassilv0111@gmail.com>
2025-02-22 00:19:53 -0300

    First version stock page

--- src/main/java/front/PegStockFrontController.java
+++ src/main/java/front/PegStockFrontController.java
@@ -3,36 +3,46 @@ package front;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
 public class PegStockFrontController {
 
     @FXML
-    private Button buttonBuy;
+    private Button buttonBuyList;
 
     @FXML
     private Button buttonDashboard;
 
     @FXML
-    private Button buttonEstoque;
+    private Button buttonHome;
 
     @FXML
-    private Button buttonHome;
+    private Button buttonLoan;
+
+    @FXML
+    private Button buttonStock;
 
     @FXML
     private Text nameOut;
 
     @FXML
-    void nameChange(MouseEvent event) {
-        // Dependendo do botão clicado, o nome do botão será exibido
+    private AnchorPane stockPage;
+
+    @FXML
+    void pages(MouseEvent event) {
+        // Dependendo do botão clicado, o nome do botão será exibido no Text e a Page muda
         if (event.getSource() == buttonHome) {
-            nameOut.setText(buttonHome.getText());
+            buttonHome.setOnMouseClicked(event1 -> nameOut.setText(buttonHome.getText()));
         } else if (event.getSource() == buttonDashboard) {
-            nameOut.setText(buttonDashboard.getText());
-        } else if (event.getSource() == buttonEstoque) {
-            nameOut.setText(buttonEstoque.getText());
-        } else if (event.getSource() == buttonBuy) {
-            nameOut.setText(buttonBuy.getText());
+            buttonDashboard.setOnMouseClicked(event1 -> nameOut.setText(buttonDashboard.getText()));
+        } else if (event.getSource() == buttonStock) {
+            buttonStock.setOnMouseClicked(event1 -> nameOut.setText(buttonStock.getText()));
+            buttonStock.addEventHandler(MouseEvent.MOUSE_CLICKED, event1 -> stockPage.setVisible(true));
+        } else if (event.getSource() == buttonBuyList) {
+            buttonBuyList.setOnMouseClicked(event1 -> nameOut.setText(buttonBuyList.getText()));
+        } else if (event.getSource() == buttonLoan) {
+            buttonLoan.setOnMouseClicked(event1 -> nameOut.setText(buttonLoan.getText()));
         }
     }
 
