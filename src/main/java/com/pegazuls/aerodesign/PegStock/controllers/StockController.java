a0d133258e0e140ee805aa694f78f89c3e9324dc
lauramoroni <moronilaura28@gmail.com>
2025-03-10 16:27:15 -0300

    feat: implement material registration functionality in StockController

--- src/main/java/com/pegazuls/aerodesign/PegStock/controllers/StockController.java
+++ src/main/java/com/pegazuls/aerodesign/PegStock/controllers/StockController.java
@@ -1,8 +1,12 @@
 package com.pegazuls.aerodesign.PegStock.controllers;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -52,34 +56,34 @@ public class StockController implements Initializable {
     private TableColumn<Material, LocalDate> expiryColumn;
 
     @FXML
-    private Button buttonBuyList;
-
-    @FXML
-    private Button buttonDashboard;
-
-    @FXML
-    private Button buttonHome;
-
-    @FXML
-    private Button buttonLoan;
-
-    @FXML
-    private Button buttonNavigate;
-
-    @FXML
-    private Button buttonRegisterBuyList;
-
-    @FXML
-    private Button buttonRegisterStock;
-
-    @FXML
-    private Button buttonStock;
-
-    @FXML
     private ImageView imageView;
 
     @FXML
-    private Text nameOut;
+    private TextField registerCod;
+
+    @FXML
+    private TextField registerName;
+
+    @FXML
+    private TextField registerQuantity;
+
+    @FXML
+    private TextField registerDescription;
+
+    @FXML
+    private DatePicker registerCreatedDate;
+
+    @FXML
+    private DatePicker registerExpirationDate;
+
+    @FXML
+    private ChoiceBox<Category> registerCategory;
+
+    @FXML
+    private ChoiceBox<Box> registerBox;
+
+    @FXML
+    private Button buttonRegisterStock;
 
     @FXML
     private AnchorPane registerStockPage;
@@ -87,28 +91,83 @@ public class StockController implements Initializable {
     @FXML
     private AnchorPane registerStockPageBackground;
 
-    @FXML
-    private AnchorPane stockPage;
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        // Configura as colunas da tabela
         productColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
         categoryColumn.setCellValueFactory(new PropertyValueFactory<>("category"));
         cabinetColumn.setCellValueFactory(new PropertyValueFactory<>("box"));
         expiryColumn.setCellValueFactory(new PropertyValueFactory<>("expirationDate"));
 
-        if (materialService != null) {
-            System.out.println("MaterialService is not null");
-            loadMaterials();
-        } else {
-            System.out.println("MaterialService is null");
-        }
+        // Carrega os materiais na tabela
+        loadMaterials();
+
+        // Configura as opções dos ChoiceBox
+        registerCategory.setItems(FXCollections.observableArrayList(Category.values()));
+        registerBox.setItems(FXCollections.observableArrayList(Box.values()));
     }
 
     private void loadMaterials() {
         List<Material> materials = materialService.findAll();
         materialTable.getItems().setAll(materials);
+    }
+
+    @FXML
+    void showRegisterStockPage(MouseEvent event) {
+        registerStockPageBackground.setVisible(true);
+        registerStockPage.setVisible(true);
+    }
+
+    @FXML
+    void registerMaterial(MouseEvent event) {
+        String name = registerName.getText();
+        Long cod = Long.parseLong(registerCod.getText());
+        int quantity = Integer.parseInt(registerQuantity.getText());
+        String description = registerDescription.getText();
+        LocalDate createdDate = registerCreatedDate.getValue();
+        LocalDate expirationDate = registerExpirationDate.getValue();
+        Category category = registerCategory.getValue();
+        Box box = registerBox.getValue();
+
+        Material material = new Material();
+        material.setName(name);
+        material.setCod(cod);
+        material.setQuantity(quantity);
+        material.setDescription(description);
+        material.setCreatedDate(createdDate);
+        material.setExpirationDate(expirationDate);
+        material.setCategory(category);
+        material.setBox(box);
+
+        materialService.create(material);
+
+        // Atualiza a tabela
+        loadMaterials();
+
+        // Fecha o formulário de cadastro
+        registerStockPageBackground.setVisible(false);
+        registerStockPage.setVisible(false);
+
+        clearForm();
+    }
+
+    private void clearForm() {
+        registerName.clear();
+        registerCod.clear();
+        registerQuantity.clear();
+        registerDescription.clear();
+        registerCreatedDate.setValue(null);
+        registerExpirationDate.setValue(null);
+        registerCategory.setValue(null);
+        registerBox.setValue(null);
+    }
+
+    @FXML
+    void cancelRegister(MouseEvent event) {
+        registerStockPageBackground.setVisible(false);
+        registerStockPage.setVisible(false);
+        clearForm();
     }
 
     @FXML
@@ -121,11 +180,5 @@ public class StockController implements Initializable {
             Image image = new Image(file.toURI().toString());
             imageView.setImage(image);
         }
-    }
-
-    @FXML
-    void showRegisterStockPage(MouseEvent event) {
-        buttonRegisterStock.setOnMouseClicked(event1 -> registerStockPageBackground.setVisible(true));
-        buttonRegisterStock.addEventHandler(MouseEvent.MOUSE_CLICKED, event1 -> registerStockPage.setVisible(true));
     }
 }
