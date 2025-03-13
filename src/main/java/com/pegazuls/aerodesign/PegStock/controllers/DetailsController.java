ae8155aa02bfa6145b86735e953a087e8d07d30e
Lucas Silva de Souza <lucassilv0111@gmail.com>
2025-03-12 21:22:02 -0300

    Finish details page

--- src/main/java/com/pegazuls/aerodesign/PegStock/controllers/DetailsController.java
+++ src/main/java/com/pegazuls/aerodesign/PegStock/controllers/DetailsController.java
@@ -3,57 +3,25 @@ package com.pegazuls.aerodesign.PegStock.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
 public class DetailsController {
 
     @FXML
-    private Button buttonAdd;
+    private Button buttonAdd, buttonBorrowing, buttonCancel, buttonConsumption, confirm, buttonCancelAdd;
 
     @FXML
-    private Button buttonBorrowing;
+    private Button buttonCancelConsume, buttonConfirmAdd, buttonConfirmConsume, buttonCancelBorrowing, buttonConfirmBorrowing;
 
     @FXML
-    private Button buttonCancel;
+    private Text code, dateAdd, dateConsumption, dateCriation, description, nameBox, nameCategory;
 
     @FXML
-    private Button buttonConsumption;
+    private Text nameTitle, nameValid, quantity, totalAdd, totalConsumption;
 
     @FXML
-    private Text code;
-
-    @FXML
-    private Text dateAdd;
-
-    @FXML
-    private Text dateConsumption;
-
-    @FXML
-    private Text dateCriation;
-
-    @FXML
-    private Text description;
-
-    @FXML
-    private Text nameBox;
-
-    @FXML
-    private Text nameCategory;
-
-    @FXML
-    private Text nameTitle;
-
-    @FXML
-    private Text nameValid;
-
-    @FXML
-    private Text quantity;
-
-    @FXML
-    private Text totalAdd;
-
-    @FXML
-    private Text totalConsumption;
+    private AnchorPane registerBackgroundPage, registerConsumePage, registerAddPage, registerBorrowingPage;
 
     @FXML
     void add(MouseEvent event) {
@@ -71,8 +39,31 @@ public class DetailsController {
     }
 
     @FXML
+    void confirm(MouseEvent event) {
+
+    }
+
+    @FXML
     void consumption(MouseEvent event) {
 
+    }
+
+    @FXML
+    void showRegisterConsumePage(MouseEvent event) {
+        registerBackgroundPage.setVisible(true);
+        registerConsumePage.setVisible(true);
+    }
+
+    @FXML
+    void showRegisterAddPage(MouseEvent event) {
+        registerBackgroundPage.setVisible(true);
+        registerAddPage.setVisible(true);
+    }
+
+    @FXML
+    void showRegisterBorrowingPage(MouseEvent event) {
+        registerBackgroundPage.setVisible(true);
+        registerBorrowingPage.setVisible(true);
     }
 
 }
