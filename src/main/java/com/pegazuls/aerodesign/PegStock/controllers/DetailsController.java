642ec611345d7ea16d40c145993a4996d67db20f
ClaraLeticia <claralet1346@gmail.com>
2025-03-12 21:28:34 -0300

    feat: show material detail

--- src/main/java/com/pegazuls/aerodesign/PegStock/controllers/DetailsController.java
+++ src/main/java/com/pegazuls/aerodesign/PegStock/controllers/DetailsController.java
@@ -2,6 +2,7 @@ package com.pegazuls.aerodesign.PegStock.controllers;
 
 import com.pegazuls.aerodesign.PegStock.model.entities.Material;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
@@ -9,8 +10,11 @@ import front.ScreenManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @Controller
-public class DetailsController {
+public class DetailsController implements Initializable {
 
     private Material material;
 
@@ -18,52 +22,19 @@ public class DetailsController {
     private ScreenManager screenManager;
 
     @FXML
-    private Button buttonAdd;
+    private Button buttonAdd, buttonBorrowing, buttonCancel, buttonConsumption;
 
     @FXML
-    private Button buttonBorrowing;
+    private Text code, dateAdd, dateConsumption, dateCriation, description,
+            nameBox, nameCategory, nameTitle, nameValid, quantity,
+            totalAdd, totalConsumption;
 
-    @FXML
-    private Button buttonCancel;
-
-    @FXML
-    private Button buttonConsumption;
-
-    @FXML
-    private Text code;
-
-    @FXML
-    private Text dateAdd;
-
-    @FXML
-    private Text dateConsumption;
-
-    @FXML
-    private Text dateCriation;
-
-    @FXML
-    private Text description;
-
-    @FXML
-    private Text nameBox;
-
-    @FXML
-    private Text nameCategory;
-
-    @FXML
-    private Text nameTitle;
-
-    @FXML
-    private Text nameValid;
-
-    @FXML
-    private Text quantity;
-
-    @FXML
-    private Text totalAdd;
-
-    @FXML
-    private Text totalConsumption;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if (material != null) {
+            updateUi();
+        }
+    }
 
     public void showDetails(Material material) {
         this.material = material;
@@ -71,26 +42,29 @@ public class DetailsController {
         screenManager.changeScreen("/front/fxml/DetailsPage.fxml");
     }
 
-
-
-    @FXML
-    void add(MouseEvent event) {
-
+    private void updateUi() {
+        if (material != null && nameTitle != null) { // Verifica se os elementos foram carregados
+            nameTitle.setText(material.getName());
+            code.setText(material.getCod().toString());
+            description.setText(material.getDescription());
+            nameCategory.setText(material.getCategory().toString());
+            nameBox.setText(material.getBox().toString());
+            quantity.setText(String.valueOf(material.getQuantity()));
+            nameValid.setText(material.getExpirationDate().toString());
+            dateCriation.setText(material.getCreatedDate().toString());
+            totalConsumption.setText(String.valueOf(material.getConsumerQuantity()));
+        }
     }
 
     @FXML
-    void borrowingPage(MouseEvent event) {
-
-    }
+    void add(MouseEvent event) {}
 
     @FXML
-    void cancel(MouseEvent event) {
-
-    }
+    void borrowingPage(MouseEvent event) {}
 
     @FXML
-    void consumption(MouseEvent event) {
+    void cancel(MouseEvent event) {}
 
-    }
-
+    @FXML
+    void consumption(MouseEvent event) {}
 }
