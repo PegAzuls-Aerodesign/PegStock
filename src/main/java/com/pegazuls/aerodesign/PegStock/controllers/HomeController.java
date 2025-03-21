f91a488e27667c0a19b73a802dfd84ed20feaf7e
Lucas Silva de Souza <lucassilv0111@gmail.com>
2025-03-21 16:21:53 -0300

    Home page

---
+++ src/main/java/com/pegazuls/aerodesign/PegStock/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.pegazuls.aerodesign.PegStock.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+    @FXML
+    private Text numberBASTAO_COLA_QUENTE, numberCOLA, numberDESEMPENHO, numberELETRICA, numberEPI_TI;
+
+    @FXML
+    private Text numberESTANTE, numberFERRAMENTAS, numberFERRAMENTAS_CORTANTES, numberFERRAMENTAS_ELETRICAS;
+
+    @FXML
+    private Text numberFITAS, numberLIMPEZA,numberLIXAS, numberOUTROS, numberPALITOS, numberPARAFUSO_DERIVADOS;
+
+}
