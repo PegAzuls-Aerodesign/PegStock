f2a4afc90c2a8cf441943c39b83e977323fefb58
joas-barros <joas.barros14@hotmail.com>
2025-03-25 10:06:57 -0300

    new: branch to summarise information in dashboard page

--- src/main/java/com/pegazuls/aerodesign/PegStock/controllers/DashboardController.java
+++ src/main/java/com/pegazuls/aerodesign/PegStock/controllers/DashboardController.java
@@ -1,14 +1,19 @@
 package com.pegazuls.aerodesign.PegStock.controllers;
 
+import com.pegazuls.aerodesign.PegStock.service.MaterialService;
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class DashboardController {
+
+    @Autowired
+    private MaterialService materialService;
 
     @FXML
     private Button buttonRelatorio;
@@ -27,5 +32,9 @@ public class DashboardController {
 
     @FXML
     private Text numberDisponivel;
+
+    private void getMostConsumedMaterial() {
+
+    }
 
 }
