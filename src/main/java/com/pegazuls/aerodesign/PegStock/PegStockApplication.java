fa418adebce76ce9a113ee86335d0acdf83ba0c3
lauramoroni <moronilaura28@gmail.com>
2025-06-09 18:35:40 -0300

    remove: abolishing javafx

--- src/main/java/com/pegazuls/aerodesign/PegStock/PegStockApplication.java
+++ src/main/java/com/pegazuls/aerodesign/PegStock/PegStockApplication.java
@@ -2,10 +2,8 @@ package com.pegazuls.aerodesign.PegStock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"front", "com.pegazuls.aerodesign.PegStock"})
 public class PegStockApplication {
 
     public static void main(String[] args) {
