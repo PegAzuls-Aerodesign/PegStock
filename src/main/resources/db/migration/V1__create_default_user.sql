9eae98ff3e1a624baef98aac0894f83ad00a4e4c
joas-barros <joas.barros14@hotmail.com>
2025-07-09 09:10:43 -0300

    feat: flyway migrations

---
+++ src/main/resources/db/migration/V1__create_default_user.sql
@@ -0,0 +1,3 @@
+-- Email : pegazuls@ufersa.edu.br Password: pegstock
+INSERT INTO tb_user (name, email, password) VALUES
+    ('PegAzuls', 'pegazuls@ufersa.edu.br', '$2a$10$Qut7kGdi9ZP6N6zpmekT9OcfnPEhJJIC1On1oUOhU8ClHdEMKgWyu');
