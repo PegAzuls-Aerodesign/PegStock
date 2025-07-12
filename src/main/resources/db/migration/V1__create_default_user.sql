a829746275be704910a4b8c4966fbc67924d9af9
vynijales <vynijales@gmail.com>
2025-07-12 04:22:23 -0300

    feat: add user table creation migration

--- src/main/resources/db/migration/V1__create_default_user.sql
+++ src/main/resources/db/migration/V1__create_default_user.sql
@@ -1,3 +1,10 @@
 -- Email : pegazuls@ufersa.edu.br Password: pegstock
-INSERT INTO tb_user (name, email, password) VALUES
-    ('PegAzuls', 'pegazuls@ufersa.edu.br', '$2a$10$Qut7kGdi9ZP6N6zpmekT9OcfnPEhJJIC1On1oUOhU8ClHdEMKgWyu');
+-- INSERT INTO tb_user (name, email, password) VALUES
+--     ('PegAzuls', 'pegazuls@ufersa.edu.br', '$2a$10$Qut7kGdi9ZP6N6zpmekT9OcfnPEhJJIC1On1oUOhU8ClHdEMKgWyu');
+
+CREATE TABLE tb_user (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
