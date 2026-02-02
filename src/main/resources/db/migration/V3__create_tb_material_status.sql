a29fa82961327ab4bd68ece84c216f80435f2ed9
joas-barros <joas.barros14@hotmail.com>
2026-02-02 17:08:45 -0300

    fix: create new migrations file

--- src/main/resources/db/migration/V3__create_tb_material_status.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE tb_material_status (
-    material_id BIGINT NOT NULL,
-    status VARCHAR(50),
-    CONSTRAINT fk_material FOREIGN KEY (material_id) REFERENCES tb_material (cod) ON DELETE CASCADE
-);
--- src/main/resources/db/migration/V5__alter_tb_shopping_list_add_material_id.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE tb_shopping_list ADD COLUMN material_cod BIGINT;
-ALTER TABLE tb_shopping_list
-ADD CONSTRAINT fk_shopping_list_material
-FOREIGN KEY (material_cod) REFERENCES tb_material(cod);
